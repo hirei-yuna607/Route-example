d5396bee8f1ff7cbccd4041950220d7da9335a0b
hirei-yuna607 <77141336+hirei-yuna607@users.noreply.github.com>
2021-07-05 05:51:24 +0900

    next-commit-state

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = useLocation();
+  const { state } = useLocation();
+  console.log(location);
+  console.log(state);
   return (
     <div>
       <h1>Page1DetailA</h1>
