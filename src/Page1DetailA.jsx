e49ef2b13b9e47e42c250bf29d40c6fcbbb3952f
hirei-yuna607 <77141336+hirei-yuna607@users.noreply.github.com>
2021-07-05 05:57:50 +0900

    next-commit-history

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,13 +1,19 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation();
   const { state } = useLocation();
   console.log(location);
   console.log(state);
+
+  const history = useHistory();
+  const onClickBack = () => {
+    history.goBack();
+  };
   return (
     <div>
       <h1>Page1DetailA</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
