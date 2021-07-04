a6174da0cd3456ae713db3eac3ff88497c9df092
hirei-yuna607 <77141336+hirei-yuna607@users.noreply.github.com>
2021-07-04 22:23:28 +0900

    next commit query

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -7,6 +7,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2</h1>
       <Link to="/page2/1000">URL Parameter</Link>
+      <br />
+      <Link to="/page2/1000?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
